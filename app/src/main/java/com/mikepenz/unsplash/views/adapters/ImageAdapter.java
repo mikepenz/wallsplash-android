0bc9df8d07f7b8a3278a51d9299527382369b288
Mike Penz <mikepenz@gmail.com>
2015-01-19 21:23:18 +0100

    * use picasso in Adapter and animate the image set

--- app/src/main/java/com/mikepenz/unsplash/views/adapters/ImageAdapter.java
+++ app/src/main/java/com/mikepenz/unsplash/views/adapters/ImageAdapter.java
@@ -1,23 +1,27 @@
 package com.mikepenz.unsplash.views.adapters;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.support.v7.graphics.Palette;
 import android.support.v7.widget.RecyclerView;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.koushikdutta.async.future.FutureCallback;
-import com.koushikdutta.ion.ImageViewBitmapInfo;
-import com.koushikdutta.ion.Ion;
 import com.mikepenz.unsplash.OnItemClickListener;
 import com.mikepenz.unsplash.R;
 import com.mikepenz.unsplash.models.Image;
 import com.mikepenz.unsplash.views.Utils;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
 import java.util.ArrayList;
 
@@ -59,55 +63,67 @@ public class ImageAdapter extends RecyclerView.Adapter<ImagesViewHolder> {
         final Image currentImage = images.get(position);
         imagesViewHolder.imageAuthor.setText(currentImage.getAuthor());
         imagesViewHolder.imageDate.setText(currentImage.getReadableDate());
-        imagesViewHolder.imageView.setDrawingCacheEnabled(true);
+        //imagesViewHolder.imageView.setDrawingCacheEnabled(true);
 
-        Ion.with(context)
-                .load(images.get(position).getImage_src())
-                .intoImageView(imagesViewHolder.imageView)
-                .withBitmapInfo()
-                .setCallback(new FutureCallback<ImageViewBitmapInfo>() {
-                    @Override
-                    public void onCompleted(Exception e, ImageViewBitmapInfo result) {
+        Picasso.with(context).load(images.get(position).getImage_src()).into(new Target() {
+            @Override
+            public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                imagesViewHolder.imageView.setImageBitmap(bitmap);
 
-                        if (e == null && result != null && result.getBitmapInfo() != null && result.getBitmapInfo().bitmap != null) {
+                Animation myFadeInAnimation = AnimationUtils.loadAnimation(context, R.anim.alpha_on);
+                imagesViewHolder.imageView.startAnimation(myFadeInAnimation);
 
-                            Palette.generateAsync(result.getBitmapInfo().bitmap, new Palette.PaletteAsyncListener() {
+                if (bitmap != null) {
+                    Palette.generateAsync(bitmap, new Palette.PaletteAsyncListener() {
+                        @Override
+                        public void onGenerated(Palette palette) {
+
+                            Palette.Swatch s = palette.getVibrantSwatch();
+                            if (s == null) {
+                                s = palette.getMutedSwatch();
+                            }
+
+                            if (s != null) {
+                                imagesViewHolder.imageAuthor.setTextColor(s.getTitleTextColor());
+                                imagesViewHolder.imageDate.setTextColor(s.getTitleTextColor());
+                            }
+
+                            if (Build.VERSION.SDK_INT >= 21) {
+                                imagesViewHolder.imageView.setTransitionName("cover" + position);
+                            }
+                            imagesViewHolder.imageTextContainer.setOnClickListener(new View.OnClickListener() {
                                 @Override
-                                public void onGenerated(Palette palette) {
-
-                                    Palette.Swatch s = palette.getVibrantSwatch();
-                                    if (s == null) {
-                                        s = palette.getMutedSwatch();
-                                    }
-
-                                    if (s != null) {
-                                        imagesViewHolder.imageAuthor.setTextColor(s.getTitleTextColor());
-                                        imagesViewHolder.imageDate.setTextColor(s.getTitleTextColor());
-                                    }
-
-                                    if (Build.VERSION.SDK_INT >= 21) {
-                                        imagesViewHolder.imageView.setTransitionName("cover" + position);
-                                    }
-                                    imagesViewHolder.imageTextContainer.setOnClickListener(new View.OnClickListener() {
-                                        @Override
-                                        public void onClick(View v) {
-                                            onItemClickListener.onClick(v, position);
-                                        }
-                                    });
-
-                                    if (s != null) {
-                                        Utils.animateViewColor(imagesViewHolder.imageTextContainer, defaultBackgroundColor, s.getRgb());
-                                    }
+                                public void onClick(View v) {
+                                    onItemClickListener.onClick(v, position);
                                 }
                             });
+
+                            if (s != null) {
+                                Utils.animateViewColor(imagesViewHolder.imageTextContainer, defaultBackgroundColor, s.getRgb());
+                            }
                         }
-                    }
-                });
+                    });
+                }
+            }
+
+            @Override
+            public void onBitmapFailed(Drawable errorDrawable) {
+                imagesViewHolder.imageView.setImageDrawable(errorDrawable);
+            }
+
+            @Override
+            public void onPrepareLoad(Drawable placeHolderDrawable) {
+                imagesViewHolder.imageView.setImageDrawable(null);
+            }
+        });
+
+        //calculate height of the list-item so we don't have jumps in the view
+        DisplayMetrics displaymetrics = context.getResources().getDisplayMetrics();
+        imagesViewHolder.imageView.setMinimumHeight((int) (displaymetrics.widthPixels / images.get(position).getRatio()));
     }
 
     @Override
     public int getItemCount() {
-
         return images.size();
     }
 }
@@ -136,9 +152,7 @@ class ImagesViewHolder extends RecyclerView.ViewHolder implements View.OnClickLi
 
     @Override
     public void onClick(View v) {
-
         onItemClickListener.onClick(v, getPosition());
-
     }
 }
 
