dc2a1f8a3ce5209b204a29679dd89036c459ddad
Mike Penz <mikepenz@gmail.com>
2015-01-22 20:46:52 +0100

    * pass colors to the detailActivity so they don'T have to be generated again

--- app/src/main/java/com/mikepenz/unsplash/fragments/ImagesFragment.java
+++ app/src/main/java/com/mikepenz/unsplash/fragments/ImagesFragment.java
@@ -137,6 +137,8 @@ public class ImagesFragment extends Fragment {
             Intent detailIntent = new Intent(getActivity(), DetailActivity.class);
             detailIntent.putExtra("position", position);
             detailIntent.putExtra("selected_image", selectedImage);
+            detailIntent.putExtra("swatch_title_text_color", selectedImage.getSwatch().getTitleTextColor());
+            detailIntent.putExtra("swatch_rgb", selectedImage.getSwatch().getRgb());
 
             ImageView coverImage = (ImageView) v.findViewById(R.id.item_image_img);
             if (coverImage == null) {
